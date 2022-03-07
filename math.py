5305a1f89cd9a29c94740f8de420a44288370e77
shreyakute <62518131+shreyakute@users.noreply.github.com>
2022-03-07 15:28:43 +0530

    added math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#add implement
+def add(x,y):
+    return x+y
+
+#sub implement 
+def sub(x,y):
+    return x-y #on master branch
+
+#mul implement
+def mul(x,y):
+    return x*y #on Bug456
+
+#div implement
+def div(x,y):
+    pass
